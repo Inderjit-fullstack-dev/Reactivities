cb9bed28567ede40a167a156d67c224ccd4ce4b0
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2020-09-28 15:51:09 +0530

    activity Detail api implementation

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Activities;
@@ -12,15 +13,12 @@ namespace API.Controllers
     public class ActivitiesController : ControllerBase
     {
         private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
+        public ActivitiesController(IMediator mediator) => _mediator = mediator;
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List() 
-        {
-            return await _mediator.Send(new List.Query());
-        }
+        public async Task<ActionResult<List<Activity>>> List() => await _mediator.Send(new List.Query());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> Details(Guid id) => await _mediator.Send(new Details.Query() { Id = id });
     }
 }
