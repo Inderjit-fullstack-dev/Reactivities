f4e71f288e450c1afd50bd167e0dae42bae91010
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2020-09-29 11:29:19 +0530

    create & edit activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -13,12 +13,34 @@ namespace API.Controllers
     public class ActivitiesController : ControllerBase
     {
         private readonly IMediator _mediator;
-        public ActivitiesController(IMediator mediator) => _mediator = mediator;
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
 
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List() => await _mediator.Send(new List.Query());
+        public async Task<ActionResult<List<Activity>>> List()
+        {
+            return await _mediator.Send(new List.Query());
+        }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> Details(Guid id) => await _mediator.Send(new Details.Query() { Id = id });
+        public async Task<ActionResult<Activity>> Details(Guid id) 
+        {
+            return await _mediator.Send(new Details.Query() { Id = id });
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await _mediator.Send(command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
     }
 }
