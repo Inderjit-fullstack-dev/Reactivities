52c7dba5d5bb6b80cfaa5dbdd0d4c84ea1d5d009
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2020-09-30 15:31:43 +0530

    adding agent for api hit in react app

---
+++ client-app/src/api/agent.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosResponse } from "axios";
+
+axios.defaults.baseURL = "http://localhost:5000/api";
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string) => {
+    axios.get(url).then(responseBody);
+  },
+  post: (url: string, body: any) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: any) => axios.put(url, body).then(responseBody),
+  del: (url: string) => axios.delete(url).then(responseBody),
+};
+
+const Activities = {
+  list: () => requests.get("/activities"),
+  details: (id: string) => requests.get(`/activities/${id}`),
+  create: (activity: any) => requests.post("/activities", activity),
+  update: (activity: any) =>
+    requests.put(`/activities/${activity.id}`, activity),
+  delete: (id: string) => requests.del(`/activities/${id}`),
+};
+
+export default { Activities };
